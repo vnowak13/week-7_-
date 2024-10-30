02a0298c172ec2b58f4b0cd0b0d851cff110b05a
Nowak <vnowak@cps.k12.il.us>
2024-10-30 11:41:25 -0600

    added more stuff

--- learning/comparison_operators.py
+++ learning/comparison_operators.py
@@ -14,15 +14,22 @@
 
 seconds = [1.23, 1.45, 1.02]
 current = 1.11
+seconds.append(current)
+print(seconds)
+#print(seconds)
 
 # Remove item 1.45 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
+seconds.remove(1.45)
+print(seconds)
 
 # Remove items 1.45, 1.02, and 1.11 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
 
-
+for i in seconds:
+    if i == 1.45 or i == 1.02 or i == 1.11:
+        seconds.remove(i)
+print(seconds)
 ################################comparison operators#########################
 #remember....
 # > greater
@@ -37,7 +44,10 @@ seconds = [1.23, 1.45, 1.02, 1.11]
 
 # Comparison Operators Practice  1:
 # Create two variables (num1 and num2) with the following values: 36 and 17. Check if num1 is greater than or equal to num2 and store the result of that comparison in a variable called my_bool
-
+# num1 = 36
+# num2 = 17
+# my_bool = num1 >=num2
+# print(my_bool)
 
 
 # Comparison Operators Practice  2:
@@ -45,17 +55,22 @@ seconds = [1.23, 1.45, 1.02, 1.11]
 # Inside num1, store the result of the square root of 25
 # Inside num2, store the number 5.
 # Check if num1 is equal to num2 and store the result of that comparison in a variable called my_bool.
+# num1 = math.sqrt(25)
+# num2 = 5
+# my_bool = num1 == num2
+# print(my_bool)
 
-
+import math
 # Comparison Operators Practice #3:
 # Create two variables (num1 and num2):
 
 # Inside num1, store the result of 64 x 3
-
+num1 = 64*3
 # Inside num2, store the result of 24 x 8
-
+num2 = 24*8
 # Check if num1 is different from num2 and store the result of that comparison in a variable called my_bool.
-
+my_bool = num1 != num2
+print(my_bool)
 
 
 
